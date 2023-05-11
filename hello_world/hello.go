b965da1dc841d9874a9026d126f9a57b5c2c216c
lmilunovic <ladislav.milunovic@develabs.com>
2023-05-11 18:27:55 +0200

    Expand hello world

--- hello_world/hello.go
+++ hello_world/hello.go
@@ -2,10 +2,34 @@ package main
 
 import "fmt"
 
-func Hello(name string) string {
-	return "Hello, " + name
+const englishHelloPrefix = "Hello"
+const spanishHelloPrefix = "Hola"
+const frenchHelloPrefix = "Bonjour"
+const serbianHelloPrefix = "Zdravo živo"
+
+func greetingPrefix(language string) (prefix string) {
+	switch language {
+	case "French":
+		prefix = frenchHelloPrefix
+	case "Spanish":
+		prefix = spanishHelloPrefix
+	case "Serbian":
+		prefix = serbianHelloPrefix
+	default:
+		prefix = englishHelloPrefix
+	}
+	return
+}
+
+func Hello(name, language string) string {
+
+	if name == "" {
+		name = "World"
+	}
+
+	return greetingPrefix(language) + ", " + name
 }
 
 func main() {
-	fmt.Println(Hello("Go"))
+	fmt.Println(Hello("Go", ""))
 }
--- hello_world/hello_test.go
+++ hello_world/hello_test.go
@@ -3,8 +3,41 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	got := Hello("Chris")
-	want := "Hello, Chris"
+
+	t.Run("saying hello to people", func(t *testing.T) {
+		got := Hello("Chris", "")
+		want := "Hello, Chris"
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("Say hello world when empty string is passed", func(t *testing.T) {
+		got := Hello("", "")
+		want := "Hello, World"
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("Say hello in Spannish", func(t *testing.T) {
+		got := Hello("Jose", "Spanish")
+		want := "Hola, Jose"
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("Say hello in French", func(t *testing.T) {
+		got := Hello("Jaques", "French")
+		want := "Bonjour, Jaques"
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("Say hello in Serbian", func(t *testing.T) {
+		got := Hello("Živorad", "Serbian")
+		want := "Zdravo živo, Živorad"
+		assertCorrectMessage(t, got, want)
+	})
+
+}
+
+func assertCorrectMessage(t testing.TB, got, want string) {
+	t.Helper()
 
 	if got != want {
 		t.Errorf("got %q want %q", got, want)
