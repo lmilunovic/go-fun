a1953b9de794066575edd1c1ac3644c5dd7e4b2f
lmilunovic <ladislav.milunovic@develabs.com>
2023-05-16 21:10:19 +0200

    Add slice instead of fixed array

--- arrays/arrays_test.go
+++ arrays/arrays_test.go
@@ -3,11 +3,16 @@ package arrays
 import "testing"
 
 func TestArrays(t *testing.T) {
-	numbers := [5]int{1, 2, 3, 4, 5}
-	got := Sum(numbers)
-	want := 15
 
-	if got != want {
-		t.Errorf("want %d got %d, given %v", got, want, numbers)
-	}
+	t.Run("Collection of any size", func(t *testing.T) {
+		numbers := []int{1, 2, 3}
+		got := Sum(numbers)
+		want := 6
+
+		if got != want {
+			t.Errorf("Got %d, but want %d, input %v", got, want, numbers)
+		}
+
+	})
+
 }
