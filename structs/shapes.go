4788e94b858b30da12bef8874de04bcbe4002e6e
lmilunovic <ladislav.milunovic@develabs.com>
2023-05-22 21:37:53 +0200

    Add Triangle shape

--- structs/shapes.go
+++ structs/shapes.go
@@ -15,8 +15,17 @@ type Circle struct {
 	Radius float64
 }
 
+type Triangle struct {
+	Base   float64
+	Height float64
+}
+
 func (r Rectangle) Area() float64 {
 	return r.Width * r.Height
+}
+
+func (t Triangle) Area() float64 {
+	return (t.Base * t.Height) * 0.5
 }
 
 func (r Rectangle) Perimiter() float64 {
--- structs/structs_test.go
+++ structs/structs_test.go
@@ -16,27 +16,21 @@ func TestPerimiter(t *testing.T) {
 
 func TestAerea(t *testing.T) {
 
-	checkArea := func(t testing.TB, shape Shape, want float64) {
-		t.Helper()
-		got := shape.Area()
-
-		if got != want {
-			t.Errorf("got %g, but want %g", got, want)
-		}
+	areaTests := []struct {
+		shape Shape
+		want  float64
+	}{
+		{shape: Rectangle{Width: 12, Height: 6}, want: 72.0},
+		{shape: Circle{Radius: 10}, want: 314.1592653589793},
+		{shape: Triangle{Base: 12, Height: 6}, want: 36.0},
 	}
 
-	t.Run("run rectangle test", func(t *testing.T) {
-		rect := Rectangle{10.0, 10.0}
-		want := 100.0
+	for _, tt := range areaTests {
+		got := tt.shape.Area()
+		want := tt.want
 
-		checkArea(t, rect, want)
-	})
-
-	t.Run("run circle test", func(t *testing.T) {
-		circle := Circle{10}
-
-		want := 314.1592653589793
-
-		checkArea(t, circle, want)
-	})
+		if got != want {
+			t.Errorf("got %.2f, but want %.2f", got, want)
+		}
+	}
 }
